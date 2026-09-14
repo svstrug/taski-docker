f2efc620c78656d3cc65eef287f275f4a1f06752
svstrug <svstrug@gmail.com>
2026-09-15 00:41:11 +0300

    Debug false setup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['mytaski.sytes.net', '158.160.153.221', '192.168.1.230', '127.0.0.1', 'localhost']
 
